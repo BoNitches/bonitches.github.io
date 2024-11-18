b72ba2df64cf256b68ea1950bb83b61b9309336a
DiddyGames <167829286+DiddyGames@users.noreply.github.com>
2024-11-18 12:33:44 -0800

    Update subtitle.js

--- js/subtitle.js
+++ js/subtitle.js
@@ -6,10 +6,10 @@ function changeSub(num) {
 
 
 var say = [];
-say[0] = "no more free dthang";
-say[1] = "50$ for 4 methods tap in wit me @rptjerry";
-say[2] = "free lee drilly";
-say[3] = "tap in wit me for motion @rptjerry"
+say[0] = "48 hours with justin bieber or unlimited hours of free games: you choose";
+say[1] = "ok but where do you actually store 1000 bottles of baby oil";
+say[2] = "diddy rhymes with skibidi";
+say[3] = "his music lk fire tho"
 // pick a random greeting
 var howmany = say.length;
 var bRand = 0;
